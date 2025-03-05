45b429c676cf943f616847fe7a246165129d6b28
Nizami Sevindi <nizami.sevindi@cudos.ch>
2025-03-05 11:54:36 +0100

    correction

--- app.py
+++ app.py
@@ -36,12 +36,7 @@ def get_text_chunks(raw_text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    client = AzureOpenAI(
-        api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-        api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
-    )
-
+    
     embeddings = AzureOpenAIEmbeddings(
         model="text-embedding-ada-002",
         api_key=os.getenv("AZURE_OPENAI_API_KEY"),
@@ -50,15 +45,12 @@ def get_vectorstore(text_chunks):
     )
     
     vectorstore = FAISS.from_texts(embedding=embeddings, texts=text_chunks)
-    print(vectorstore)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
     endpoint = os.environ["ENDPOINT_URL"]   
     subscription_key = os.environ["AZURE_OPENAI_API_KEY"] 
-    endpoint = os.environ["ENDPOINT_URL"] 
-
 
     llm = AzureChatOpenAI(
         model="gpt-4o",
@@ -66,7 +58,6 @@ def get_conversation_chain(vectorstore):
         api_version="2024-05-01-preview",
         azure_endpoint=endpoint
     )
-
 
     memory = ConversationBufferMemory(
         memory_key="chat_history",
@@ -78,22 +69,11 @@ def get_conversation_chain(vectorstore):
         retriever=vectorstore.as_retriever(),
         memory=memory
     )
-    print(conversation_chain)
     return conversation_chain
 
-
-def handle_userinput(user_question):
-    
-    response = st.session_state.conversation({'question': user_question})
-
-    st.session_state.chat_history = response['chat_history']
-
-    history_pairs = list(zip(st.session_state.chat_history[::2], st.session_state.chat_history[1::2]))
-    history_pairs.reverse()  # En son eklenen çift en üste gelsin
-
 def handle_userinput(user_question):
 
-    response = st.session_state.conversation({'question': user_question})
+    response = st.session_state.conversation.invoke({'question': user_question})
 
     st.session_state.chat_history = response['chat_history']
 
@@ -103,7 +83,6 @@ def handle_userinput(user_question):
     for user_msg, bot_msg in history_pairs:
         st.write(user_template.replace("{{MSG}}", user_msg.content), unsafe_allow_html=True)
         st.write(bot_template.replace("{{MSG}}", bot_msg.content), unsafe_allow_html=True)
-
 
 def main():
     load_dotenv()
